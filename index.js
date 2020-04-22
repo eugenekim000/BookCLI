92766920d3a87093e3f62b6e0216124e90ca5168
eugenekim000 <eugenekim000@gmail.com>
2020-04-22 11:44:40 -0700

    added timeout to api request and reduced error messages

--- index.js
+++ index.js
@@ -3,61 +3,69 @@ const fs = require('fs');
 const colors = require('colors');
 
 const getData = async (query) => {
-  try {
-    let responseArray = [];
+	try {
+		let responseArray = [];
 
-    if (Array.isArray(query)) {
-      query = query.join('+');
-    }
-    const response = await axios.get(
-      `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=5`
-    );
+		if (Array.isArray(query)) {
+			query = query.join('+');
+		}
+		/* 		const response = await axios.get(
+			`https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=5`
+		); */
 
-    if (response.totalItems === 0) {
-      return;
-    }
-    response.data.items.map((data) => {
-      responseArray.push({
-        authors: data.volumeInfo.authors,
-        title: data.volumeInfo.title,
-        publisher: data.volumeInfo.authors,
-      });
-    });
+		const response = await axios({
+			method: 'get',
+			url: `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=5`,
+			timeout: 1000 * 15, //timeout after 15 seconds
+		}).catch(function (error) {
+			console.log('Error, timeout exceeded.');
+		});
 
-    return responseArray;
-  } catch (error) {
-    console.log(error.response);
-  }
+		if (response.totalItems === 0) {
+			return;
+		}
+		response.data.items.map((data) => {
+			responseArray.push({
+				authors: data.volumeInfo.authors,
+				title: data.volumeInfo.title,
+				publisher: data.volumeInfo.authors,
+			});
+		});
+
+		return responseArray;
+	} catch (error) {
+		console.log('Error in query.');
+	}
 };
 
 const updateReadingList = async (book, dir) => {
-  const saveDirectory = dir ? dir : '';
+	const saveDirectory = dir ? dir : '';
 
-  fs.appendFile(`${saveDirectory}readingList.txt`, `\n${book}`, function (err) {
-    if (err) throw err;
-    console.log('File is created successfully.');
-  });
+	fs.appendFile(`${saveDirectory}readingList.txt`, `\n${book}`, function (err) {
+		if (err) throw err;
+		console.log('File is created successfully.');
+	});
 };
 
 const openReadingList = (dir) => {
-  const saveDirectory = dir ? dir : '';
-  try {
-    const readingList = fs.readFileSync(
-      `${saveDirectory}readingList.txt`,
-      'utf8'
-    );
-    console.log(readingList);
-  } catch (err) {
-    if (err.code === 'ENOENT') {
-      console.log(colors.red(`Cannot find directory! $${dir}`));
-    } else {
-      console.log(colors.red(err));
-    }
-  }
+	const saveDirectory = dir ? dir : '';
+	try {
+		const readingList = fs.readFileSync(
+			`${saveDirectory}readingList.txt`,
+			'utf8'
+		);
+		console.log(readingList);
+	} catch (err) {
+		if (err.code === 'ENOENT') {
+			console.log(colors.red(`Cannot find directory! $${dir}`));
+		} else {
+			console.log(colors.red(err));
+		}
+	}
 };
 
 module.exports = {
-  getData,
-  updateReadingList,
-  openReadingList,
+	getData,
+	updateReadingList,
+	openReadingList,
 };
--- inquirer.js
+++ inquirer.js
@@ -3,43 +3,43 @@ const colors = require('colors');
 const { updateReadingList } = require('./index.js');
 
 module.exports = function (query, saveDir) {
-  inquirer
-    .prompt([
-      {
-        type: 'list',
-        name: 'save',
-        message: 'Choose query to save',
-        choices: query.map((item) => {
-          //another possible option is to push title, author, and publisher
-          //into an array, stringify it, and filter out {} and [].
-          if (item === 'Cancel') return 'Cancel';
+	inquirer
+		.prompt([
+			{
+				type: 'list',
+				name: 'save',
+				message: 'Choose query to save',
+				choices: query.map((item) => {
+					//another possible option is to push title, author, and publisher
+					//into an array, stringify it, and filter out {} and [].
+					if (item === 'Cancel') return 'Cancel';
 
-          const finalString = [];
-          item.title
-            ? finalString.push(item.title)
-            : finalString.push('No Titles Found');
+					const finalString = [];
+					item.title
+						? finalString.push(item.title)
+						: finalString.push('No Titles Found');
 
-          resultChecker(finalString, item, 'authors');
-          resultChecker(finalString, item, 'publisher');
+					resultChecker(finalString, item, 'authors');
+					resultChecker(finalString, item, 'publisher');
 
-          return finalString.join(' ');
-        }),
-      },
-    ])
+					return finalString.join(' ');
+				}),
+			},
+		])
 
-    .then(function (answers) {
-      if (answers.save === 'Cancel') {
-        console.log(colors.red('Query canceled.'));
-      } else {
-        updateReadingList(answers.save, saveDir);
-        console.log(colors.green('Query saved!'));
-      }
-    });
+		.then(function (answers) {
+			if (answers.save === 'Cancel') {
+				console.log(colors.red('Query canceled.'));
+			} else {
+				updateReadingList(answers.save, saveDir);
+				console.log(colors.green('Query saved!'));
+			}
+		});
 };
 
 function resultChecker(stringArray, result, type) {
-  stringArray.push(type.toUpperCase() + ':');
-  result[type]
-    ? result[type].map((item) => stringArray.push(item))
-    : stringArray.push(`No ${type} found.`);
+	stringArray.push(type.toUpperCase() + ':');
+	result[type]
+		? result[type].map((item) => stringArray.push(item))
+		: stringArray.push(`No ${type} found.`);
 }
